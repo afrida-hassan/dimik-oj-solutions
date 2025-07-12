bcc7e46194f5b799628211e8fe17e9d5aed3f73b
afrida-hassan <afridahassan092@gmail.com>
2025-07-12 19:19:57 +0600

    Added solution for Dimik Problem 06

---
+++ dimik 06.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int T,N,i,sum;
+    scanf("%d",&T);
+
+    for(i=0;i<T;i++)
+    {
+        scanf("%d",&N);
+        int last_digit = N%10;
+
+    for(N; N >= 10; N = N/10)
+    {
+
+    }
+    sum = N + last_digit;
+
+     printf("Sum = %d\n",sum);
+    }
+
+
+
+}
